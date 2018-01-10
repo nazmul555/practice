8aaae7821070b8aacc52e2b76f109f1852623503
Nazmul Islam <i.nazmul555@yahoo.com>
2018-01-10 18:57:15 +0600

    My practise codes here...

--- js/custom.js
+++ js/custom.js
@@ -16,3 +16,20 @@ $('#manualinputlabel').click(function(e) {
          }
 
       });
+
+
+
+
+      $(document).ready(function() {
+          var date = new Date();
+
+          var day = date.getDate();
+          var month = date.getMonth() + 1;
+          var year = date.getFullYear();
+
+          if (month < 10) month = "0" + month;
+          if (day < 10) day = "0" + day;
+
+          var today = year + "-" + month + "-" + day;
+          $("#exampleInputAmount2").attr("value", today);
+      });
