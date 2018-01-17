07e3f90cf6c9af7f2c0101b234370948c24076a7
Nazmul Islam <35252651+nazmul555@users.noreply.github.com>
2018-01-17 18:50:38 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -21,72 +21,178 @@ $('#myTabs a').click(function (e) {
 
 
 
+$( function() {
+    var availableTags = [
+      "Afghanistan",
+      "Albania",
+      "Algeria",
+      "Bahamas",
+      "Bahrain",
+      "Bangladesh",
+      "Cabo Verde",
+      "Cambodia",
+      "Cameroon",
+      "Denmark",
+      "Dominican Republic",
+      "Ecuador",
+      "Egypt",
+      "El Salvador",
+      "Fiji",
+      "Finland",
+      "France",
+      "Gambia",
+      "Georgia",
+      "Germany",
+      "Hungary",
+      "Iceland",
+      "India",
+      "Indonesia",
+      "Japan",
+      "Jordan",
+      "Kenya",
+      "Kuwait",
+      "Lebanon",
+      "Liberia",
+      "Libya",
+      "Madagascar",
+      "Malaysia",
+      "Maldives",
+      "Mali",
+      "Nepal",
+      "Netherlands",
+      "New Zealand",
+      "Oman",
+      "Pakistan",
+      "Palestine",
+      "Panama",
+      "Papua New Guinea",
+      "Qatar",
+      "Romania",
+      "Russia",
+      "Samoa",
+      "Serbia",
+      "Singapore",
+      "South Africa",
+      "South Korea",
+      "Sri Lanka",
+      "Sweden",
+      "Switzerland",
+      "Syria",
+      "Taiwan",
+      "Tajikistan",
+      "Tanzania",
+      "Thailand",
+      "Trinidad and Tobago",
+      "Tunisia",
+      "Turkey",
+      "Uganda",
+      "Ukraine",
+      "United Arab Emirates (UAE)",
+      "United Kingdom (UK)",
+      "United States of America (USA)",
+      "Uruguay",
+      "Vatican City (Holy See)",
+      "Venezuela",
+      "Vietnam",
+      "Yemen",
+      "Zambia",
+      "Zimbabwe"
+    ];
+    $( "#destination3" ).autocomplete({
+      source: availableTags
+    });
+    $( "#exampleInputAmount" ).autocomplete({
+      source: availableTags
+    });
+    $("#destination3").autocomplete("widget").addClass('fixed-height');
+    $("#exampleInputAmount").autocomplete("widget").addClass('fixed-height');
+    $("#destination3").autocomplete("widget").attr('style', 'max-height: 150px; overflow-y: auto; overflow-x: hidden;')
+    $("#exampleInputAmount").autocomplete("widget").attr('style', 'max-height: 150px; overflow-y: auto; overflow-x: hidden;')
+  });
+
+
+
+
 /*
-      $(document).ready(function() {
-          var date = new Date();
-
-          var day = date.getDate();
-          var month = date.getMonth() + 1;
-          var year = date.getFullYear();
-
-          if (month < 10) month = "0" + month;
-          if (day < 10) day = "0" + day;
-
-          var today = year + "-" + month + "-" + day;
-          $("#exampleInputAmount2").attr("value", today);
+  $( function() {
+      var availableTags = [
+        "Afghanistan",
+        "Albania",
+        "Algeria",
+        "Bahamas",
+        "Bahrain",
+        "Bangladesh",
+        "Cabo Verde",
+        "Cambodia",
+        "Cameroon",
+        "Denmark",
+        "Dominican Republic",
+        "Ecuador",
+        "Egypt",
+        "El Salvador",
+        "Fiji",
+        "Finland",
+        "France",
+        "Gambia",
+        "Georgia",
+        "Germany",
+        "Hungary",
+        "Iceland",
+        "India",
+        "Indonesia",
+        "Japan",
+        "Jordan",
+        "Kenya",
+        "Kuwait",
+        "Lebanon",
+        "Liberia",
+        "Libya",
+        "Madagascar",
+        "Malaysia",
+        "Maldives",
+        "Mali",
+        "Nepal",
+        "Netherlands",
+        "New Zealand",
+        "Oman",
+        "Pakistan",
+        "Palestine",
+        "Panama",
+        "Papua New Guinea",
+        "Qatar",
+        "Romania",
+        "Russia",
+        "Samoa",
+        "Serbia",
+        "Singapore",
+        "South Africa",
+        "South Korea",
+        "Sri Lanka",
+        "Sweden",
+        "Switzerland",
+        "Syria",
+        "Taiwan",
+        "Tajikistan",
+        "Tanzania",
+        "Thailand",
+        "Trinidad and Tobago",
+        "Tunisia",
+        "Turkey",
+        "Uganda",
+        "Ukraine",
+        "United Arab Emirates (UAE)",
+        "United Kingdom (UK)",
+        "United States of America (USA)",
+        "Uruguay",
+        "Vatican City (Holy See)",
+        "Venezuela",
+        "Vietnam",
+        "Yemen",
+        "Zambia",
+        "Zimbabwe"
+      ];
+      $( "#exampleInputAmount" ).autocomplete({
+        source: availableTags
       });
-*/
-
-/*
-      $('#calendar').calendar();
-
-      var events = [
-
-    {start: '2017-04-01', end: '2017-04-07', summary: "Event #1", mask: true},
-
-    {start: '2017-04-21', end: '2017-04-28', summary: "Event #2", mask: true},
-
-    ...
-
-];
-
-$('#calendar').calendar({
-
-  events: events
-
-});
-*/
-
-
-/*
-$(document).ready(function() {
-    var date = new Date();
-
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
-
-    if (month < 10) month = "0" + month;
-    if (day < 10) day = "0" + day;
-
-    var today = year + "-" + month + "-" + day;
-    $("#exampleInputAmount2").attr("value", today);
-});
-
-
-
-
-$(document).ready(function() {
-    var date = new Date();
-
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
-
-    if (month < 10) month = "0" + month;
-    if (day < 10) day = "0" + day;
-
-    var today = year + "-" + month + "-" + day;
-    $("#date3").attr("value", today);
-});
-*/
+    } );
+    */
